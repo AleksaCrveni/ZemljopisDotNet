c761923571ef2cfdc44c2570620ac8d51133b7c2
WickedyWick <crvenialeksa@gmail.com>
2024-07-13 23:02:54 +0200

    Dispatch proto

--- ZemljopisAPI/Services/Sockets/SocketService.cs
+++ ZemljopisAPI/Services/Sockets/SocketService.cs
@@ -11,18 +11,30 @@ public class SocketService(ILogger<SocketService> _logger) : ISocketService, ITr
     SocketData? data;
     try
     {
+      // @TODO Late on change from JSON to string buffer type
       data = JsonConvert.DeserializeObject<SocketData>(socketString);
     } catch(Exception e)
     {
       // this is basically 400 response
       return new SocketResult() { Evt = Events.ERR, Success = false };
     }
-
     if (data is null)
     {
       return new SocketResult() { Evt = Events.ERR, Success = false };
     }
 
+    await Dispatch(data);
     return new SocketResult() { Evt = data.Evt, ResponseString = "OK"};
+  }
+
+  private async Task Dispatch(SocketData data)
+  {
+    // @TODO think about what to return
+    var _ = (data.Evt) switch
+    {
+      Events.GET_ROOM_DATA => "",
+      Events.ERR => "",
+      _ => ""
+    };
   }
 }
